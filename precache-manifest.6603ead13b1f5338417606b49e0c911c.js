8c34d5b9c86030498970747a7836294ba5a0a237
Anastasiia Lukina <anlukina24@gmail.com>
2020-07-29 13:58:57 +0300

    Updates

---
+++ precache-manifest.6603ead13b1f5338417606b49e0c911c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fed4b748c0370696847c655629eaf77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f81aaf2774db33b80c76",
+    "url": "./static/css/main.1d198dce.chunk.css"
+  },
+  {
+    "revision": "2dccce6a023ac7bdcd3d",
+    "url": "./static/js/2.75343c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.75343c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81aaf2774db33b80c76",
+    "url": "./static/js/main.10ce212b.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.6603ead13b1f5338417606b49e0c911c.js"
 );
 
 self.addEventListener('message', (event) => {
